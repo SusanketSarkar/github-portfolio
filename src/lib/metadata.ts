5ef23390aa6d33b7190a5399d278a549a49efce4
SusanketSarkar <susanketsarkar1140@gmail.com>
2025-07-09 09:43:32 +0530

    [fix] update metadata image and icon paths to use absolute URLs

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -5,8 +5,8 @@ import { Metadata } from 'next'
 export function constructMetadata({
   title = 'Susanket Sarkar', // TODO: Add a custom title
   description = "I am a software engineer with a passion for building scalable and efficient web applications.", // TODO: Add a custom description
-  image = '/logo.png', // TODO: Add a custom image
-  icons = '/logo.png', // TODO: Add a custom icon
+  image = 'https://github.com/SusanketSarkar/github-portfolio/blob/main/public/logo.png', 
+  icons = 'https://github.com/SusanketSarkar/github-portfolio/blob/main/public/logo.png', // TODO: Add a custom icon
   noIndex = false
 }: {
   title?: string
